2059fc1256befc3feb78bdd9acdc0845b102c1c3
vutuyetnhung <32647960+vutuyetnhung@users.noreply.github.com>
2017-10-18 15:57:35 +0700

    Delete demo.m

--- demo.m
+++
@@ -1,8 +0,0 @@
- fprintf ( 1, '\n' );
-  fprintf ( 1, 'HELLO:\n' );
-  fprintf ( 1, '  MATLAB version\n' );
-  fprintf ( 1, '  This is how to say:\n' );
-  fprintf ( 1, '\n' );
-  fprintf ( 1, '  Hello, world!\n' );
-
-  quit
