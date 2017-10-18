e32de65a1ac509098fb9c82b829f5e25dba08798
vutuyetnhung <32647960+vutuyetnhung@users.noreply.github.com>
2017-10-18 15:40:21 +0700

    ghi chú 1:  đây chỉ là những file học tiếng thôi
    toafn file học tiếng trung

---
+++ demo.m
@@ -0,0 +1,8 @@
+ fprintf ( 1, '\n' );
+  fprintf ( 1, 'HELLO:\n' );
+  fprintf ( 1, '  MATLAB version\n' );
+  fprintf ( 1, '  This is how to say:\n' );
+  fprintf ( 1, '\n' );
+  fprintf ( 1, '  Hello, world!\n' );
+
+  quit
